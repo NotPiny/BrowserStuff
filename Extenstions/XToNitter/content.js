bcdcc10b835033351e06b844d8bc1729267ed2f3
NotPiny <NotPiny@gmail.com>
2023-09-10 00:41:17 +0100

    Add ignored routes to TwitterToNitter :D

--- Extenstions/XToNitter/content.js
+++ Extenstions/XToNitter/content.js
@@ -1,4 +1,43 @@
-// Redirect the URL
-if (window.location.hostname === 'twitter.com') {
-    window.location.href = 'https://nitter.net' + window.location.pathname; // Nitter has (almost) 1:1 URL structure so it's easy to redirect
+const ignoredPaths = [ // Paths that should be ignored
+    '/home',
+    '/compose/tweet',
+    '/settings',
+    '/notifications',
+    '/messages',
+    '/i/bookmarks'
+];
+const customMappings = [
+    {
+        twitter: '/explore',
+        nitter: '/search'
+    }
+]
+// Redirect the URL if the path is available on Nitter
+
+// Applies on website load
+if (window.location.hostname === 'twitter.com' && !ignoredPaths.includes(window.location.pathname)) {
+    // If custom mapping is available, use it
+    if (customMappings.find(mapping => mapping.twitter === window.location.pathname)) {
+        const mapping = customMappings.find(mapping => mapping.twitter === window.location.pathname);
+        window.location.href = 'https://nitter.net' + mapping.nitter;
+    }
+
+    // If custom mapping is not available, use default
+    window.location.href = 'https://nitter.net' + window.location.pathname;
 }
+
+// Applies on URL change
+window.addEventListener('click', () => {
+    // If a button or smth is clicked, check if the URL should be redirected
+
+    if (window.location.hostname === 'twitter.com' && !ignoredPaths.includes(window.location.pathname)) {
+        // If custom mapping is available, use it
+        if (customMappings.find(mapping => mapping.twitter === window.location.pathname)) {
+            const mapping = customMappings.find(mapping => mapping.twitter === window.location.pathname);
+            window.location.href = 'https://nitter.net' + mapping.nitter;
+        }
+
+        // If custom mapping is not available, use default
+        window.location.href = 'https://nitter.net' + window.location.pathname;
+    }
+})
